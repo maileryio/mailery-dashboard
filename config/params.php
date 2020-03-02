2bbff05b7d85375d273c403d2ac69817d28acef7
Igor Maliy <imaliy.filsh@gmail.com>
2020-03-02 12:03:25 +0200

    Initial commit

--- config/params.php
+++ config/params.php
@@ -20,7 +20,7 @@ use Yiisoft\Router\UrlGeneratorInterface;
 return [
     'router' => [
         'routes' => [
-            Route::get('/brand/{brandId:\d+}', [DefaultController::class, 'index'])
+            '/dashboard/default/index' => Route::get('/dashboard/default/index', [DefaultController::class, 'index'])
                 ->name('/dashboard/default/index'),
         ],
     ],
