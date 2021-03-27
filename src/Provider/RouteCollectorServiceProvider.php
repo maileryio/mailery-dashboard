1221a61fb3b8676e774592d65f18e246b09d920a
Igor Maliy <imaliy.filsh@gmail.com>
2021-03-27 09:57:07 +0200

    Development

--- src/Provider/RouteCollectorServiceProvider.php
+++ src/Provider/RouteCollectorServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Mailery\Dashboard\Provider;
 
-use Yiisoft\Di\Container;
+use Psr\Container\ContainerInterface;
 use Yiisoft\Di\Support\ServiceProvider;
 use Yiisoft\Router\RouteCollectorInterface;
 use Yiisoft\Router\Route;
@@ -10,7 +10,7 @@ use Mailery\Dashboard\Controller\DefaultController;
 
 final class RouteCollectorServiceProvider extends ServiceProvider
 {
-    public function register(Container $container): void
+    public function register(ContainerInterface $container): void
     {
         /** @var RouteCollectorInterface $collector */
         $collector = $container->get(RouteCollectorInterface::class);
