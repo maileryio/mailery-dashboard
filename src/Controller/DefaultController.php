7581f251ecc5452c937b069db9f9a7d0b6d4facd
Igor Maliy <imaliy.filsh@gmail.com>
2021-08-26 10:52:11 +0300

    Development

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -30,7 +30,7 @@ class DefaultController
     {
         $this->viewRenderer = $viewRenderer
             ->withController($this)
-            ->withViewBasePath(dirname(dirname(__DIR__)) . '/views');
+            ->withViewPath(dirname(dirname(__DIR__)) . '/views');
     }
 
     /**
